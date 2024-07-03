c86301b700f365015990e928e73fe8d8effebf41
Ercument <ercukalkann@gmail.con>
2024-07-03 15:06:38 +0300

    inserting post (without a creator)

--- graphql/root.js
+++ graphql/root.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
@@ -78,6 +79,42 @@ module.exports = {
         return {
             token: token,
             userId: user._id.toString()
+        };
+    },
+
+    createPost: async function ({ postInput }, req) {
+        const errors = [];
+
+        if (validator.isEmpty(postInput.title) ||
+            !validator.isLength(postInput.title, { min: 5 })) {
+            errors.push({ message: 'title is invalid' });
+        }
+
+        if (validator.isEmpty(postInput.content) ||
+            !validator.isLength(postInput.content, { min: 5 })) {
+            errors.push({ message: 'content is invalid' });
+        }
+
+        if (errors.length > 0) {
+            const error = new Error('invalid input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+
+        const newPost = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl
+        });
+
+        const createdPost = await newPost.save();
+
+        return {
+            ...createdPost._doc,
+            _id: createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString()
         };
     }
 }
